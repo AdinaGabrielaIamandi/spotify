19fd85ad0ab5835618b566743193aba64474ba06
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-02-24 12:59:31 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { NavbarLeft } from "./components/NavbarLeft";
+import { Footerbar } from "./components/Footerbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="conatiner-fluid">
+      <BrowserRouter>
+        <div className="row">
+          <NavbarLeft />
+          <Routes>
+            <Route />
+          </Routes>
+        </div>
+        <Footerbar />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavbarTop.jsx
@@ -0,0 +1,13 @@
+export const NavbarTop = () => {
+  return (
+    <div class="row">
+      <div class="col-9 col-lg-11 mainLinks d-none d-md-flex">
+        <a href="/">TRENDING</a>
+        <a href="/">PODCAST</a>
+        <a href="/">MOODS AND GENRES</a>
+        <a href="/">NEW RELEASES</a>
+        <a href="/">DISCOVER</a>
+      </div>
+    </div>
+  );
+};
